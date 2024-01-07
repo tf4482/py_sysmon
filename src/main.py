8707e155a5587d763a92581dad592f5379a40550
Tobi F. <tf4482@gmail.com>
2024-01-07 02:57:26 +0100

    Implemented get_cpu_usage function

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import psutil
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
+
+def get_cpu_usage():
+    return psutil.cpu_percent(interval=1)
+
+
 class SystemMonitorWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("System Monitor")
+        self.setWindowTitle("py_sysmon")
         self.setGeometry(100, 100, 600, 400)
         self.label = QLabel("CPU Usage: ", self)
         self.label.move(20, 20)
         self.label.resize(200, 20)
-
-
